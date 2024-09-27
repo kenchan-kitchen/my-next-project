01b75eb7640fbb24672e8069e02818357d71e872
kenchan <ken.letsroll.econ.pc@gmail.com>
2024-09-27 15:19:30 +0900

    end of section4

--- app/_components/NewsList/index.tsx
+++ app/_components/NewsList/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import styles from "./index.module.css";
 import Category from "../Category";
 import Date from "../Date";
@@ -13,29 +14,29 @@ return <p>記事がありません。</p>;
 }
 return (
 <ul>
-{news.map((article) => (
-<li key={article.id} className={styles.list}>
-<div className={styles.link}>
-<Image
-className={styles.image}
-src="/no-image.png"
-alt="No Image"
-width={1200}
-height={630}
-/>
-<dl className={styles.content}>
-<dt className={styles.title}>{article.title}</dt>
-<dd className={styles.meta}>
+    {news.map((article) => (
+        <li key={article.id} className={styles.list}>
+            <Link href={`news/${article.id}`} className={styles.link}>
+                <Image
+                className={styles.image}
+                src="/no-image.png"
+                alt="No Image"
+                width={1200}
+                height={630}
+                />
+                <dl className={styles.content}>
+                    <dt className={styles.title}>{article.title}</dt>
+                        <dd className={styles.meta}>
 
-<Category category={article.category} />
-<Date date={article.publishedAt ??
-article.createdAt} />
+                        <Category category={article.category} />
+                        <Date date={article.publishedAt ??
+                        article.createdAt} />
 
-</dd>
-</dl>
-</div>
-</li>
-))}
+                        </dd>
+                </dl>
+            </Link>
+        </li>
+    ))}
 </ul>
 );
 }
